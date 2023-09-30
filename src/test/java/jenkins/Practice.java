dcca28aecb95f834fd18dd35c8e514d6e8978343
Tanya-priyadarshini <priyadarshinetanya@gmail.com>
2023-09-30 17:55:40 +0530

    First commit

---
+++ src/test/java/jenkins/Practice.java
@@ -0,0 +1,19 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	@Test
+	public void practice() {
+		System.out.println("practice");
+	}
+	@Test
+	public void practice1() {
+		System.out.println("practice1");
+	}
+	@Test
+	public void practice2() {
+		System.out.println("practice2");
+	}
+
+}
